ef42dd10425ba336c1957b503ec5a2165528ceed
diefersontsx <appsigmadev@gmail.com>
2024-03-07 22:30:06 -0800

    feat: config header.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
+import Header from "@/components/header";
+
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex flex-row items-center justify-center p-24">
-      <ThemeToggle />
+    <main className="flex flex-row items-center justify-center">
+      <Header />
 
     </main>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,17 @@
+import { ThemeToggle } from "./theme-toggle";
+
+
+
+
+
+
+
 export default function Header() {
   return (
-    
+    <div className="container flex flex-row items-center justify-between mt-4">
+      <h1 className="font-bold text-lg text-gray-50 ">MyStudies</h1>
+
+      <ThemeToggle />
+    </div>
   )
 }
